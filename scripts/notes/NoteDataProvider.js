1fcbb9d9674cd5d129b9c98682cac6634f3116a2
Mark McCann <mjmccann12@me.com>
2020-03-18 10:45:09 -0500

    completed chapt six

---
+++ scripts/notes/NoteDataProvider.js
@@ -0,0 +1,37 @@
+let notes = []
+
+const eventHub = document.querySelector(".container")
+
+const dispatchStateChangeEvent = () => {
+    const noteStateChangedEvent = new CustomEvent("noteStateChanged")
+
+    eventHub.dispatchEvent(noteStateChangedEvent)
+}
+
+/*
+    Allow other modules to get a copy of the application state
+*/
+export const useNotes = () => notes.sort((c, n) => n.timestamp - c.timestamp).slice()
+
+/*
+    Get the state of the notes from the API into the application
+*/
+export const getNotes = () => {
+    return fetch('http://localhost:8088/notes')
+        .then(response => response.json())
+        .then(parsedNotes => {
+            notes = parsedNotes
+        })
+}
+
+export const saveNote = note => {
+    return fetch('http://localhost:8088/notes', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(note)
+    })
+        .then(getNotes)
+        .then(dispatchStateChangeEvent)
+}
---
+++ scripts/notes/NoteForm.js
@@ -0,0 +1,55 @@
+import { saveNote } from "./NotesProvider.js"
+
+const contentTarget = document.querySelector(".noteFormContainer")
+const eventHub = document.querySelector(".container")
+
+let visibility = false
+
+eventHub.addEventListener("noteFormButtonClicked", customEvent => {
+    visibility = !visibility
+
+    if (visibility) {
+        contentTarget.classList.remove("invisible")
+    }
+    else {
+        contentTarget.classList.add("invisible")
+    }
+})
+
+// Handle browser-generated click event in component
+contentTarget.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "saveNote") {
+
+        const noteText = document.querySelector("#noteText").value
+        const criminalName = document.querySelector("#criminal").value
+
+        // Make a new object representation of a note
+        const newNote = {
+            noteText: noteText,
+            criminal: criminalName,
+            timestamp: Date.now()
+        }
+
+        // Change API state and application state
+        saveNote(newNote)
+    }
+})
+
+const render = () => {
+    contentTarget.classList.add("invisible")
+    contentTarget.innerHTML = `
+        <fieldset>
+            <label class="label label--notes" for="noteText">Note:</label>
+            <textarea id="noteText"></textarea>
+        </fieldset>
+        <fieldset>
+            <label class="label label--notes" for="criminal">Criminal:</label>
+            <input type="text" id="criminal">
+        </fieldset>
+        <button id="saveNote">Save Note</button>
+    `
+}
+
+export const NoteForm = () => {
+    render()
+}
